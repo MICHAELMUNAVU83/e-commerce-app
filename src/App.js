811626a5d8df83bda46065ccf2bddc9f04ae3c6f
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-01-08 16:23:40 +0300

    Added react router dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import Products from './components/Products';
 function App() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setProducts(data);
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+     
     </div>
   );
 }
